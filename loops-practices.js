310453ec44a5fe53a9a1c6ea6671f1c7a0d0795e
MD AAkil Raiyan Prantho <prantho98@gmail.com>
2023-08-05 19:20:32 +0600

    Module 18 Files

--- loops-practices.js
+++ loops-practices.js
@@ -26,3 +26,69 @@ while(i <= 456)
     }
     i++;
 }
+
+// 4
+
+for ( i = 581; i<=623; i++)
+{
+    if( i % 2 != 0)
+    {
+        console.log(i);
+    }
+}
+
+
+// 5
+/* Differentiation Table
+Basis of Comparison	For Loop	While Loop
+Keyword	Uses for keyword	Uses while keyword
+Used	For loop is used when the number of iterations is already known.	While loop is used when the number of iterations is already Unknown.
+absence of condition	The loop runs infinite times in the absence of condition	Returns the compile time error in the absence of condition
+Nature of Initialization	Once done, it cannot be repeated	In the while loop, it can be repeated at every iteration.
+Functions	To iterate, the range or xrange function is used.	There is no such function in the while loop.
+Initialization based on iteration	To be done at the beginning of the loop.	In the while loop, it is possible to do this anywhere in the loop body.
+Generator Support	Python's for loop can iterate over generators.	While loops cannot be directly iterated on Generators.
+Speed	The for loop is faster than the while loop.	While loop is relatively slower as compared to for loop. */
+
+// 6
+
+var topics = ['HTML','CSS','Tailwind','DaisyUI','Bootstrap','JavaScript','Variables','Arrays','Conditionals','Loops'];
+for ( i = 0; i < topics.length; i++)
+{
+    console.log(topics[i]);
+}
+
+
+// 7
+
+var mobile = ['Siemens','Motorola','Maximus','Techno','CK','Symphony','Nokia','Samsung','Xiaomi','Redmi'];
+
+i = 0;
+while(i < mobile.length)
+{
+    console.log(mobile[i]);
+    i++;
+}
+
+// 8
+
+for ( var i = 30; i < 86; i++)
+{
+    if(i > 44)
+    {
+        break;
+    }
+    console.log(i);
+}
+
+// 9
+
+var bookPrices = [ 150, 300, 490, 30, 1005, 210, 190];
+for(var i = 0; i < bookPrices.length; i++)
+{
+    if(bookPrices[i] > 200)
+    {
+        continue;
+    }
+    console.log(bookPrices[i]);
+}
---
+++ questions-list.js
@@ -0,0 +1,38 @@
+/* Variable Questions
+1.	What is JavaScript?
+2.	How does JS Work?
+3.	What is Variables?
+4.	Declare a Variable
+5.	Types of Variables. How can you find out the type of a Variable?
+6.	Primitive and Non-Primitive Data Types
+7.	Naming convention of JS variables
+8.	Math Operations   +, -, *, /, %
+9.	Short Hand  +=, -=, *=, /=
+10.	++, --
+11.	parseInt, parseFloat
+12.	toFixed 
+
+Array Questions
+1.	What is the purpose?
+2.	How to declare an array in JS?
+3.	Number of elements in an Array
+4.	What is index?
+5.	Find the value of an element by index
+6.	Change an element by index
+7.	Get the index of an element by the value
+8.	What does it mean when you get undefined the value of an element
+9.	How can you add an element to an array at the last position?
+10.	How can you remove the last element from array?
+11.	Add an element at the first position of an array
+12.	Remove the first element of an array
+
+Conditional Questions
+1.	Meaning of: >, <, >=, <=, ==, !=, ===, !==
+2.	Meaning of && and ||
+3.	Let’s say you have x amount of money
+a.	 If you have more than 80,000, you’ll buy a Desktop
+b.	If you have more than 60,000, you’ll buy gaming laptop
+c.	If you have more than 40,000, you’ll buy Lenovo Yoga
+d.	If you have more than 20,000, you’ll buy a Used Laptop
+e.	Otherwise you’ll buy a Mobile Phone
+ */
